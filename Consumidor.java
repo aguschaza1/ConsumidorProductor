daac617f14ada59f02870057fb37a198b10a5a1c
aguschaza1 <63989958+aguschaza1@users.noreply.github.com>
2020-04-20 00:09:38 -0300

    Update Consumidor.java

--- Consumidor.java
+++ Consumidor.java
@@ -1,1 +1,23 @@
 
+Public class Consumidor extends Thread {
+  private Buffer buffer;
+  
+  public Consumidor(Buffer buffer) {this.buffer = buffer;  }
+  
+  public Consumidor(Buffer buffer) {
+        this.buffer = buffer;
+    }
+
+    public void run() {
+        for(int i = 0; i < 20; ++i) {
+            char valor = this.buffer.recoger();
+            System.out.println(i + " Consumidor: " + valor);
+
+            try {
+                sleep(100L);
+            } catch (InterruptedException var4) {
+            }
+        }
+
+    }
+}
