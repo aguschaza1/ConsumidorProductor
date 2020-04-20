cce8ec25a841f5db39a201e6cefb00e14c3a5924
matialmar7 <63989663+matialmar7@users.noreply.github.com>
2020-04-20 00:02:55 -0300

    Update Buffer.java

--- Buffer.java
+++ Buffer.java
@@ -1,1 +1,30 @@
 
+public class Buffer {
+	
+	private int content;
+	private int maxContent;
+	private int minContent;
+	
+	public Buffer() {
+		content = 0;
+		maxContent = 25;
+		minContent = 0;
+	}
+	
+	public int getContent() {
+		return content;
+	}
+	public boolean isFull() {
+		return content == maxContent;
+	}
+	public boolean isEmpty() {
+		return content == minContent;
+	}
+	public synchronized void pushElement() {
+		content++;
+	}
+	public synchronized void popElement() {
+		content--;
+	}
+}
+
