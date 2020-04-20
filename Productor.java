70ad5b5fec9b680efa8dc7ac606e24c92e2b206d
Francisco De la Cruz <63804936+FranciscoCross@users.noreply.github.com>
2020-04-19 23:45:39 -0300

    Update Productor.java

--- Productor.java
+++ Productor.java
@@ -1,19 +1,1 @@
-package productorConsumidor;
 
-public class Productor extends Thread {
-		private Buffer buffer;
-		private final String letras="abcdefghijklmnopqrstuvxyz";
-		public Productor(Buffer buffer) {
-			this.buffer=buffer;
-		}		
-		public void run() {
-			for(int i=0; i<30; i++) {
-				char c=letras.charAt((int)(Math.random()*letras.length()));
-				buffer.poner(c);
-				System.out.println(i+" Productor: " + c);
-				try {
-					sleep(400);
-				} catch (InterruptedException e) {  }
-		} 
-	}
-}
