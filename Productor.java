997faa5df8e564cde8d6b52d31688ddffcb9ecb4
aguschaza1 <63989958+aguschaza1@users.noreply.github.com>
2020-04-20 00:32:14 -0300

    blablab

--- Productor.java
+++ Productor.java
@@ -1,1 +1,24 @@
+package productorConsumidor;
 
+public class Productor extends Thread {
+    private Buffer buffer;
+    private final String letras = "abcdefghijklmnopqrstuvxyz";
+
+    public Productor(Buffer buffer) {
+        this.buffer = buffer;
+    }
+
+    public void run() {
+        for(int i = 0; i < 30; ++i) {
+            char c = "abcdefghijklmnopqrstuvxyz".charAt((int)(Math.random() * (double)"abcdefghijklmnopqrstuvxyz".length()));
+            this.buffer.poner(c);
+            System.out.println(i + " Productor: " + c);
+
+            try {
+                sleep(400L);
+            } catch (InterruptedException var4) {
+            }
+        }
+
+    }
+}
