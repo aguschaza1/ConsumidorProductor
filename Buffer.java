3fa6237d4d227370ed636a06d019d156a37f2955
aguschaza1 <63989958+aguschaza1@users.noreply.github.com>
2020-04-23 19:25:45 -0300

    buffer
    agarrenlo con cuidado esto porque no se que onda, use una pila para guardar las cosas, la linea 32 no se que puto error tieney no se que mas puede estar mal :)

--- Buffer.java
+++ Buffer.java
@@ -1,2 +1,50 @@
+import java.util.Stack;
+import java.util.concurrent.Semaphore;
 
-Hola mati puto de mierda
+public class Buffer {
+
+    private Stack content;
+
+    private Semaphore mutex = new Semaphore(1, true);
+    private Semaphore hayData = new Semaphore(0, true);
+    private Semaphore haySpace = new Semaphore(25, true);
+
+    public Buffer() {
+        content = new Stack();
+    }
+
+    public char pull() {
+        try {
+            hayData.acquire();
+            mutex.acquire();
+            char x = (char) content.pop();
+            System.out.println("Consumidor toma:" + x + "\n");
+
+
+            System.out.println("Se libero el Consumidor\n");
+            haySpace.release();
+            mutex.release();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+
+        }
+
+        public void put(char value) {
+            haySpace.tryAcquire();
+            try {
+                mutex.acquire();
+
+                content.push(value);
+                System.out.println("Productor ingresa:");
+                System.out.println(value);
+                System.out.println("\n");
+
+                System.out.println("Se libero el Productor\n");
+                hayData.release();
+                mutex.release();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
